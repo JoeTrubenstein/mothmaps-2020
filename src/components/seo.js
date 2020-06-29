2334edb2e9e85b2ed9d5492875effb018c9ad72e
Joseph Trubenstein <josephtrubenstein@gmail.com>
2020-06-29 23:28:46 +0900

    first prod

--- src/components/seo.js
+++ src/components/seo.js
@@ -4,7 +4,7 @@ import { Helmet } from "react-helmet";
 function SEO(){
     return(
         <Helmet>
-        <title>MothMaps</title>
+        <title>The MothMaps Project</title>
         <meta name="description" content="MothMaps is a project dedicated to the collection and curation of MothMan sightings." />
         <meta name="keywords" content="mothman, cryptid, indrid cold, cryptozoology, moth, mothmap, mothmaps" />
       </Helmet>
